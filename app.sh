2758e16c1ada422d673a1ee1d0625634eaecb996
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-03-20 05:53:14 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -7,6 +7,7 @@
 # Define a collection of websites to check IP
 # Each service provides a simple endpoint that returns the current public IP.
 ip_services="
+    eth0.me
     ifconfig.icu/x-real-ip
     ifconfig.me
     ipinfo.io/ip
